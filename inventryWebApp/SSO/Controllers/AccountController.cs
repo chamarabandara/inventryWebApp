d7b6f2a10ccc5bc030c1fd96286af60d1206fcaf
unknown <bandara.chamara@gmail.com>
2021-06-09 16:59:07 +0530

    sample SSO commit

---
+++ inventryWebApp/SSO/Controllers/AccountController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace SSO_Main.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [AllowAnonymous]
+        public ActionResult Login(string returnUrl)
+        {
+            if (Request.IsAuthenticated)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            ViewBag.ReturnUrl = returnUrl;
+            return View();
+        }
+
+        [AllowAnonymous]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(string username, string password, string returnUrl)
+        {
+            if (FormsAuthentication.Authenticate(username, password))
+            {
+                FormsAuthentication.SetAuthCookie(username, false);
+                if (!string.IsNullOrEmpty(returnUrl))
+                {
+                    return Redirect(returnUrl);
+                }
+                else
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+            }
+            else
+            {
+                ModelState.AddModelError(string.Empty, "Invalid login details");
+                ViewBag.ReturnUrl = returnUrl;
+                return View();
+            }
+        }
+    }
+}
